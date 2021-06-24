844791c588df91563775e46c0f4fbc10b0ed91df
Arrun1 <arrunx18choi@gmail.com>
2021-06-24 01:49:54 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'legends-of-the-past';
+  title = 'Legends of the past';
 }
