50f0a25a11059f0e00cb022f44f4f6734dd2e0a9
Arrun1 <arrunx18choi@gmail.com>
2021-06-25 17:04:12 +0100

    third commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Legend } from '../legend';
+import { LEGENDS } from '../mock-legends';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  legend: Legend ={
-    id: 1,
-    name: 'King Arthur'
-  };
 
-
+  legends = LEGENDS;
+  selectedLegend?: Legend;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  
+  onSelect(legend: Legend): void {
+    this.selectedLegend = legend;
+  }
 }
---
+++ src/app/mock-legends.ts
@@ -0,0 +1,16 @@
+import { Legend } from './legend';
+
+export const LEGENDS: Legend[] = [
+    { id: 11, name : 'Lu Bu' },
+    { id: 12, name : 'Oda Nobunaga'},
+    { id: 13, name : 'Lancelot'},
+    { id: 14, name : 'Sasaki Koujiro'},
+    { id: 15, name : 'King Arthur'},
+    { id: 16, name : 'King Leonidas'},
+    { id: 17, name : 'Hannibal Barca'},
+    { id: 18, name : 'Gilgamesh'},
+    { id: 19, name : 'Achilles'},
+    { id: 20, name : 'Sun Tzu'}
+
+
+];
